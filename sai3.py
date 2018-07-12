b7427de1a84cb063c8a6ec77ea714680b2021013
saishiva1012 <40817045+saishiva1012@users.noreply.github.com>
2018-07-12 14:24:33 +0530

    Update sai3.py

--- sai3.py
+++ sai3.py
@@ -1,8 +1,8 @@
 a=input()
 if (a.isalpha()):
 	if a=='a' or a=='e' or a=='i' or a=='o' or a=='u' or a=='A' or a=='E' or a=='I' or a=='O' or a=='U' :
-		print("vowel")
+		print("Vowel")
 	else:
-		print("consonant")
+		print("Consonant")
 else:
 	print("invalid")
